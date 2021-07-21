5a366af16d8135de360722d9b5337b9ce624f56a
Tonya_Hales <61618434+Tonya209@users.noreply.github.com>
2021-07-21 22:54:36 +0200

    Update MiddleCharacter.java

--- src/MiddleCharacter.java
+++ src/MiddleCharacter.java
@@ -1,3 +1,8 @@
+/*In this program we need to get the middle character of a given String.
+If the number of letters is even, we will show 2 middle characters. 
+For example, if a word is "TABLE" we will show "B", and if the word is "RICE", 
+we will show "IC".*/
+
 import java.util.Scanner;
 
 public class MiddleCharacter {
@@ -6,14 +11,14 @@ public class MiddleCharacter {
         System.out.println("Introduce la palabra: ");
         String palabra = sc.nextLine();
         
-        if(palabra.length()%2==0){ //mama, potato
+        if(palabra.length()%2==0){ 
             int index= palabra.length()/2;
             System.out.println(palabra.substring(index-1, index+1));
         }else{
            int index= palabra.length()/2;
             System.out.println(palabra.substring(index, index+1));
             
-            //table
+            
         }
     
     }
